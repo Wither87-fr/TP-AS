93883d1300e4bbcf5941d57f37fcba36a5919042
Beyrand Alexis <beyrand18@iutinfo-104-08.unilim.fr>
2021-03-05 11:58:37 +0100

    Fin d'heure

--- TP3.2/app/src/main/java/com/example/listvciew/MainActivity.kt
+++ TP3.2/app/src/main/java/com/example/listvciew/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.listvciew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var filmISaw = arrayOf("Retour vers le futur (1/2/3)",
+            "Men In Black (1/2)",
+            "La ligne verte",
+            "Supercondriaque",
+            "La bande à picsou le film",
+            "Batman beyond",
+            "Saw")
+        var liste : ListView = findViewById(R.id.liste)
+        val adapter : ArrayAdapter<String> = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            filmISaw
+        )
+
+        liste.adapter = adapter
     }
 }
