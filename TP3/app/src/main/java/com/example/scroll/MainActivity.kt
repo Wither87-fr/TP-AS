b6e07df00c9a006f1d4788c80998545c382f7f3c
Beyrand Alexis <beyrand18@iutinfo-104-08.unilim.fr>
2021-03-05 11:06:10 +0100

    premier commit

---
+++ TP3/app/src/main/java/com/example/scroll/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.scroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import java.lang.StringBuilder
+import kotlin.math.PI
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val txt : TextView = findViewById(R.id.txt)
+        var txt_texte : String = txt.text.toString()
+        var i = 0
+        while (i < 100) {
+            txt_texte = StringBuilder().append(txt_texte).append(PI * i).toString()
+            i++
+        }
+        txt_texte = StringBuilder().append(txt_texte).append("-FIN").toString()
+        txt.text = txt_texte
+    }
+}
