4d0fa3b8974aad63c82633c77fbcdb25b3abe5b5
__Wither <jn.beyrand@gmail.com>
2021-03-17 11:25:50 +0100

    Debut TP3.4

---
+++ TP 3.4/app/src/main/java/fr/unilim/iut/get/from/gallery/and/camera/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.unilim.iut.get.from.gallery.and.camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
